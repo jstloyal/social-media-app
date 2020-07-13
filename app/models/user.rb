131248a8a31919a7236e8b59c956ecb605cdc73a
jstloyal <jstloyalty@gmail.com>
2020-07-13 17:55:25 +0100

    fix some lint errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,21 +13,21 @@ class User < ApplicationRecord
   has_many :inverse_friendships, class_name: :Friendship, foreign_key: :friend_id
 
   def friends
-    friends_array = friendships.map{ |friendship| friendship.friend if friendship.status }
-    friends_array + inverse_friendships.map{ |friendship| friendship.user if friendship.status }
+    friends_array = friendships.map { |friendship| friendship.friend if friendship.status }
+    friends_array + inverse_friendships.map { |friendship| friendship.user if friendship.status }
     friends_array.compact
   end
 
   def pending_friends
-    friendships.map{ |friendship| friendship.friend if !friendship.status }.compact
+    friendships.map { |friendship| friendship.friend unless friendship.status }.compact
   end
 
   def friend_requests
-    inverse_friendships.map{ |friendship| friendship.user if !friendship.status }.compact
+    inverse_friendships.map { |friendship| friendship.user unless friendship.status }.compact
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find{ |friendship| friendship.user == user }
+    friendship = inverse_friendships.find { |friend| friend.user == user }
     friendship.status = true
     friendship.save
   end
