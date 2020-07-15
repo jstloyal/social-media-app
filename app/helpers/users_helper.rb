1bb32123cd37b7b6d100f800817f7814cd5fff57
jstloyal <jstloyalty@gmail.com>
2020-07-15 18:50:36 +0100

    fix rubocop errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,14 +3,12 @@ module UsersHelper
     if @user_requests.include?(user)
       link_to 'Accept request', add_friend_path(friend_id: user.id), method: :post, class: 'btn'
       link_to 'Reject request', remove_friend_path(user_id: user.id), method: :delete, class: 'btn'
-    else
-      if @pending_requests.include?(user)
-        link_to 'Request Pending', '#'
-      elsif current_user.friend?(user)
-        link_to 'Unfriend', remove_friend_path(user_id: user.id), method: :delete, class: 'btn'
-      elsif user != current_user
-        link_to 'Friend request', friendships_path(friend_id: user.id), method: :post, class: 'btn'
-      end
+    elsif @pending_requests.include?(user)
+      link_to 'Request Pending', '#'
+    elsif current_user.friend?(user)
+      link_to 'Unfriend', remove_friend_path(user_id: user.id), method: :delete, class: 'btn'
+    elsif user != current_user
+      link_to 'Friend request', friendships_path(friend_id: user.id), method: :post, class: 'btn'
     end
   end
 end
