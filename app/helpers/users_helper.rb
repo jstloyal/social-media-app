9bcaf798a22fd77c4933a8d16634c58b239292b0
jstloyal <jstloyalty@gmail.com>
2020-07-23 11:23:16 +0100

    Add Desline button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,14 +3,21 @@ module UsersHelper
     return if current_user == user
 
     if @user_requests.include?(user)
-      link_to 'Decline friend request', remove_friend_path(user_id: user.id), method: :delete
-      link_to 'Accept friend request', add_friend_path(friend_id: user.id), method: :post
+      link_to 'Accept', add_friend_path(friend_id: user.id), method: :post
     elsif @friends.include?(user)
       link_to 'Unfriend', remove_friend_path(user_id: user.id), method: :delete
     elsif @pending_requests.include?(user)
       link_to 'Request Pending', '#'
     elsif user != current_user
       link_to 'Friend request', friendships_path(friend_id: user.id), method: :post, class: 'btn'
+    end
+  end
+
+  def decline_button(user)
+    return if current_user == user
+
+    if @user_requests.include?(user)
+      link_to 'Decline', remove_friend_path(user_id: user.id), method: :delete
     end
   end
 
